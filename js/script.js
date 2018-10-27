b49c876e36985d9d133006feb2d870b85cf987db
Mikman83 <ivlev.michael@gmail.com>
2018-10-27 17:02:42 +0300

    Написал стили для шапки мобильная версия

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$('.promo').vide('video/ocean');
+
+$(document).ready(function() {
+
+	$('.main-nav__toggle').on('click', function(e) {
+		e.preventDefault;
+		$(this).toggleClass('main-nav__toggle--active');
+		$('.main-nav').toggleClass('main-nav--active');
+	});
+	
+
+});
